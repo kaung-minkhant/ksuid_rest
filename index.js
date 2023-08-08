60327e7d1da8b7f20037cd4464ce6a3cb7b64cef
kaung-minkhant <kaungminkhant.official@gmail.com>
2023-08-08 10:54:05 +0630

    added /once end point

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const ksuid = require('ksuid')
 const app = express()
 const PORT = 3000;
 
+
+
 app.get('/', (req, res) => {
-    res.send("hello world")
+    res.send("Welcome to REST API for KSUID")
+})
+
+app.get('/once', (req, res) => {
+    const code = ksuid.randomSync()
+    const code_string = code.string
+    res.send(code_string)
 })
 
 app.listen(PORT, () => {
