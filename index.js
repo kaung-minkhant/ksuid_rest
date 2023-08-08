54f11b4bcc3872a529fd806812f39313a2aec220
kaung-minkhant <kaungminkhant.official@gmail.com>
2023-08-08 11:02:07 +0630

    added cors

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const ksuid = require('ksuid')
+const cors = require('cors')
 
 const app = express()
 const PORT = 3000;
 
 
-
-app.get('/', (req, res) => {
+app.get('/', cors(), (req, res) => {
     res.send("Welcome to REST API for KSUID")
 })
 
-app.get('/once', (req, res) => {
+app.get('/once', cors(), (req, res) => {
     const code = ksuid.randomSync()
     const code_string = code.string
     res.send(code_string)
